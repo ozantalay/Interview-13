73ab0922c22b8e749635b00c57df87fc16a5ef29
ozantalay <ozntalay@hotmail.com>
2024-09-28 23:49:32 +0300

    baslangic

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  return <CustomProgram />;
+}
+
+const half = (number) => number / 2;
+const double = (number) => number * 2;
+const increment = (number) => number + 1;
+const decrement = (number) => number - 1;
+
+const CustomProgram = () => {
+  // KODUNUZU BURAYA EKLEYİN
+};
+
+export default App;
